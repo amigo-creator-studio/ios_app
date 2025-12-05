421d5d9646ca4224fecb4b2d5a4a8f835d75d9e5
Lu Amigo <a9003112@gmail.com>
2025-12-05 22:22:02 +0800

    Add files via upload

---
+++ FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Amigo Lu on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
